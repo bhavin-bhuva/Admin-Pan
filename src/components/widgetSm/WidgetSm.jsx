ec731ac8635b14c2f6a798901c7c5207d950e0ee
Bhavin Bhuva <bhavin.b@heyarrow.com>
2022-07-27 20:16:43 +0530

    fixed overflow of widget small

--- src/components/widgetSm/WidgetSm.jsx
+++ src/components/widgetSm/WidgetSm.jsx
@@ -3,56 +3,75 @@ import "./widgetSm.css";
 import { Visibility } from "@material-ui/icons";
 
 export default function WidgetSm() {
+  const userData = [
+    {
+      fullName: "Mabel Klein",
+      profileUrl: "https://xsgames.co/randomusers/avatar.php?g=male",
+      title: "Software Engineer",
+    },
+    {
+      fullName: "Randall Ruiz",
+      profileUrl: "https://xsgames.co/randomusers/avatar.php?g=female",
+      title: "Backend Developer",
+    },
+    {
+      fullName: "Marguerite Richardson",
+      profileUrl: "https://i.pravatar.cc/300",
+      title: "Quality Engineer",
+    },
+    {
+      fullName: "Mabel Klein",
+      profileUrl: "https://xsgames.co/randomusers/avatar.php?g=male",
+      title: "Software Engineer",
+    },
+    {
+      fullName: "Randall Ruiz",
+      profileUrl: "https://xsgames.co/randomusers/avatar.php?g=female",
+      title: "Backend Developer",
+    },
+    {
+      fullName: "Marguerite Richardson",
+      profileUrl: "https://i.pravatar.cc/300",
+      title: "Quality Engineer",
+    },
+    {
+      fullName: "Mabel Klein",
+      profileUrl: "https://xsgames.co/randomusers/avatar.php?g=male",
+      title: "Software Engineer",
+    },
+    {
+      fullName: "Randall Ruiz",
+      profileUrl: "https://xsgames.co/randomusers/avatar.php?g=female",
+      title: "Backend Developer",
+    },
+    {
+      fullName: "Marguerite Richardson",
+      profileUrl: "https://i.pravatar.cc/300",
+      title: "Quality Engineer",
+    },
+  ];
+
   return (
     <div className="widgetSm">
       <span className="widgetSmTitle">New Join Members</span>
+
       <ul className="widgetSmList">
-        <li className="widgetSmListItem">
-          <img
-            src="https://xsgames.co/randomusers/avatar.php?g=female"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Vincent Ford</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" /> Display
-          </button>
-        </li>
-      </ul>
-      <ul className="widgetSmList">
-        <li className="widgetSmListItem">
-          <img
-            src="https://xsgames.co/randomusers/avatar.php?g=female"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Mildred Barker</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" /> Display
-          </button>
-        </li>
-      </ul>
-      <ul className="widgetSmList">
-        <li className="widgetSmListItem">
-          <img
-            src="https://xsgames.co/randomusers/avatar.php?g=female"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Bernard Holloway</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" /> Display
-          </button>
-        </li>
+        {userData.map((val) => {
+          return (
+            <li className="widgetSmListItem">
+              <div className="widgetSmUserInfo">
+                <img src={val.profileUrl} alt="" className="widgetSmImg" />
+                <div className="widgetSmUser">
+                  <span className="widgetSmUsername">{val.fullName}</span>
+                  <span className="widgetSmUserTitle">{val.title}</span>
+                </div>
+              </div>
+              <button className="widgetSmButton">
+                <Visibility className="widgetSmIcon" /> Display
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
